baa25e7a5c61735515c4e77ed6880871c07ccb53
Ahmed79723 <ashrafahmed797@gmail.com>
2024-03-02 21:38:43 +0200

    Third Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -108,7 +108,7 @@ export default function Navbar() {
               </li>
               <li>
                 <NavLink className="nav-link" onClick={signOut}>
-                  SignOut
+                  {localStorage.getItem("tkn") ? "SignOut" : "log In"}
                 </NavLink>
               </li>
             </ul>
